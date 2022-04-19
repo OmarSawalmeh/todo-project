1e763711eeec1c723d49a2d11a17efebf0264992
OmarSawalmeh <sawalmehomar10@gmail.com>
2022-04-19 20:32:27 +0300

    Add New Functions To js

--- app.js
+++ app.js
@@ -20,3 +20,35 @@ if(!welMsg){
         alert(`Welcome ${name} in website`);
     }
 }
+
+/* Task 6 .......... */
+var qustions = [prompt("Are You a Fan of The Premier Leaque?"), prompt("Do you like travelling?"),
+            prompt("Are You a Fan of Summer")];
+
+
+function checkQ1(){
+    if (qustions[0].toLowerCase() !== "yes" && qustions[0].toLowerCase() !== "no"){
+        qustions[0] = "invalid";
+    }
+}
+
+function checkQ2(){
+    if (qustions[1].toLowerCase() !== "yes" && qustions[1].toLowerCase() !== "no"){
+        qustions[1] = "invalid";
+    }
+}
+
+function checkQ3(){
+    if (qustions[2].toLowerCase() !== "yes" && qustions[2].toLowerCase() !== "no"){
+        qustions[2] = "invalid";
+    }
+}
+
+checkQ1();
+checkQ2();
+checkQ3();
+console.log(qustions);
+
+for(var i=0; i<qustions.length; i++){
+    console.log(qustions[i]);
+}
